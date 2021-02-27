8ddd7ece8d51765e81c323fdac32e17d516379e6
stevenhughes08 <stevenhughes08@gmail.com>
2021-02-27 15:21:35 -0500

    added js code

--- js/site.js
+++ js/site.js
@@ -1,1 +1,16 @@
 var controller = new ScrollMagic.Controller();
+
+var pinIntroScene = new ScrollMagic.Scene({
+        triggerElement: '.trigger',
+        triggerHook: 0.2,
+        duration: '150%'
+    })
+    .setPin('.trigger')
+    .addTo(controller);
+
+//GSap animation
+var scene = new ScrollMagic.Scene({
+    triggerElement: ".trigger"
+})
+
+.setTween('#buttons', 1, { opacity: 0 }).addIndicators({ name: '1 (duration: 0)' }).addTo(controller);
