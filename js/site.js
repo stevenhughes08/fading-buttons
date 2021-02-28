59f4d1971e305160d155c5291732b7ca06ea2f81
stevenhughes08 <stevenhughes08@gmail.com>
2021-02-28 08:51:09 -0500

    increased scrolling points status-working

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 var ctrl = new ScrollMagic.Controller();
-var $s = document.querySelector('div#buttons').first();
+var $s = document.getElementById('buttons');
 
 $s.style.opacity = 0;
 var scene1 = new ScrollMagic.Scene({
     offset: 10,
-    duration: 50
-}).addEventListener('enter leave', function(e) {
+    duration: 150
+}).on("enter leave", function(e) {
     TweenMax.to($s, 0.2, { opacity: e.type === 'enter' ? 1 : 0 });
 }).addTo(ctrl).addIndicators();
